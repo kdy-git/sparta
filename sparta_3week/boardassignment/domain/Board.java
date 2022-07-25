a742bcc591758676554c479ede4194ba438e9637
kdy-git <86644517+kdy-git@users.noreply.github.com>
2022-07-25 15:47:06 +0900

    3주차 과제

---
+++ sparta_3week/boardassignment/domain/Board.java
@@ -0,0 +1,44 @@
+package com.sparta.boardassignment.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity  // table 임을 선언하는것과 같다.
+public class Board extends Timestamped{
+
+    @Id  // Primary Key
+    @GeneratedValue(strategy = GenerationType.AUTO)  // Auto_increment
+    private Long no;
+
+    @Column(nullable = false)  // not null
+    private String title;
+
+    @Column(nullable = false)  // not null
+    private String contents;
+
+    @Column(nullable = false)  // not null
+    private String writer;
+
+    @Column(nullable = false)  // not null
+    private String password;
+
+    public Board(BoardDto boardDto) {
+        this.title = boardDto.getTitle();
+        this.contents = boardDto.getContents();
+        this.writer = boardDto.getWriter();
+        this.password = boardDto.getPassword();
+    }
+
+    public void update(BoardDto boardDto) {
+        this.title = boardDto.getTitle();
+        this.contents = boardDto.getContents();
+    }
+
+}
---
+++ sparta_3week/boardassignment/domain/BoardDto.java
@@ -0,0 +1,13 @@
+package com.sparta.boardassignment.domain;
+
+import lombok.Getter;
+
+@Getter
+public class BoardDto {
+
+    private String title;
+    private String contents;
+    private String writer;
+    private String password;
+
+}
